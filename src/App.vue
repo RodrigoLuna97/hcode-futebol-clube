915e472db0a498e404f174a1d22ceb3d410a7588
Rodrigo <rodluna97@gmail.com>
2021-11-21 18:19:15 -0300

    header, footer, section

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HcodeHeader />
+    <HcodeSection />
+    <HcodeFooter />
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HcodeHeader from './components/HcodeHeader'
+import HcodeFooter from './components/HcodeFooter'
+import HcodeSection from './components/HcodeSection'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HcodeHeader,
+    HcodeFooter,
+    HcodeSection
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css?family=Rajdhani&display=swap');
 </style>
---
+++ src/components/HcodeSectionNews.vue
@@ -0,0 +1,51 @@
+<template>
+    <section>
+        <div class="container">
+
+            <HcodeSectionNesIndividual 
+                img-name="news1.jpg"
+                img-info="Notícia 1"
+                news-title="Começam os treinos para a nova temporada"
+                news-content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quae inventore rerum nihil soluta quasi recusandae dolores a praesentium pariatur. Animi accusantium iure temporibus. Modi saepe recusandae beatae natus tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum repellendus aperiam facilis mollitia non necessitatibus, est totam hic quae similique voluptatem molestias accusamus. Distinctio iure quod inventore vitae maxime ea."
+                news-date="01/01/2020"
+
+            />
+
+            <HcodeSectionNesIndividual 
+            
+            img-name="news2.jpg"
+                img-info="Notícia 2"
+                news-title="Jogo de quarta-feira termina empatado"
+                news-content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quae inventore rerum nihil soluta quasi recusandae dolores a praesentium pariatur. Animi accusantium iure temporibus. Modi saepe recusandae beatae natus tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum repellendus aperiam facilis mollitia non necessitatibus, est totam hic quae similique voluptatem molestias accusamus. Distinctio iure quod inventore vitae maxime ea."
+                news-date="07/01/2020"
+            />
+
+            <HcodeSectionNesIndividual 
+            
+            img-name="news3.jpg"
+                img-info="Notícia 3"
+                news-title="A inauguração do novo estário será semana que vem"
+                news-content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quae inventore rerum nihil soluta quasi recusandae dolores a praesentium pariatur. Animi accusantium iure temporibus. Modi saepe recusandae beatae natus tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum repellendus aperiam facilis mollitia non necessitatibus, est totam hic quae similique voluptatem molestias accusamus. Distinctio iure quod inventore vitae maxime ea."
+                news-date="20/01/2020"
+                />
+        </div>
+    </section>
+</template>
+
+<script>
+import HcodeSectionNesIndividual from './HcodeSectionNewsIndividual'
+
+export default {
+    components: {
+        HcodeSectionNesIndividual
+    }
+}
+</script>
+
+<style scoped>
+section{
+    padding: 50px 0;
+    margin-top: 25px;
+    background-color: #F37502;
+}
+</style>
